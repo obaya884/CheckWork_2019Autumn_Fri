edb34b841a5f3eeedb5497875980bf23a60e32b1
otakumi <otakumi@m.cs.osakafu-u.ac.jp>
2019-11-29 15:47:18 +0900

    Initial Commit

---
+++ CheckWork_2019Autumn_Fri/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckWork_2019Autumn_Fri
+//
+//  Created by 大林拓実 on 2019/11/29.
+//  Copyright © 2019 Obayashi Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
