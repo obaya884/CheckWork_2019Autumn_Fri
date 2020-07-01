3cecc7113de1ac0f4181d1aff6da1515f66c93e4
obaya884 <taku3.884@gmail.com>
2020-07-01 12:12:14 +0900

    finish

--- CheckWork_2019Autumn_Fri/ViewController.swift
+++ CheckWork_2019Autumn_Fri/ViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    var number: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "0"
+        label.textColor = .black
+    }
+    
+    @IBAction func plus(){
+        number += 1
+        checkLabelColor()
+        label.text = String(number)
     }
 
+    @IBAction func minus(){
+        number -= 1
+        checkLabelColor()
+        label.text = String(number)
+    }
+
+    @IBAction func multiple(){
+        number *= 2
+        checkLabelColor()
+        label.text = String(number)
+    }
+
+    @IBAction func devide(){
+        number /= 2
+        checkLabelColor()
+        label.text = String(number)
+    }
+
+    @IBAction func clear(){
+        number = 0
+        checkLabelColor()
+        label.text = String(number)
+    }
+
+    func checkLabelColor() {
+        if number >= 10 {
+            label.textColor = .red
+        }
+        else if number <= -10 {
+            label.textColor = .blue
+        }
+        else {
+            label.textColor = .black
+        }
+    }
 
 }
 
